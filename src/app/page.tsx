44fa890ed60a45ea171491e21f84217402cbca08
Vo7kov <jegor.volkov.a@gmail.com>
2025-09-20 19:05:38 +0200

    feat(OW2-302): create Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import type { FC } from 'react';
 
+import { Logo } from '@/ui/logo/Logo';
+
 const Home: FC = () => {
-  return <div className="container">Welcome to the Home Page</div>;
+  return (
+    <div className="container">
+      <Logo />
+      Welcome to the Home Page
+    </div>
+  );
 };
 
 export default Home;
