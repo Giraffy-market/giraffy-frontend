076c263dfe8c5c2481309375ff95342478a80f55
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2025-09-23 22:18:32 +0300

    refactor (OW2-301): code corrections according to comments in the pull request

---
+++ src/modules/auth/constans/routes.ts
@@ -0,0 +1,11 @@
+export const API_ENDPOINTS_AUTH = {
+  AUTH: {
+    REGISTER: '/auth/register',
+    AUTHENTICATION_LOGIN: '/auth/jwt/login',
+    AUTHENTICATION_LOGOUT: '/auth/jwt/logout',
+    AUTHENTICATION_FORGOT: '/auth/forgot-password',
+    AUTHENTICATION_RESET: '/auth/reset-password',
+    REQUEST_VERIFY_TOKEN: '/auth/request-verify-token',
+    AUTHENTICATION_VERIFY: '/auth/verify',
+  },
+};
