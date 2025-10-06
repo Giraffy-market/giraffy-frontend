30594298d50dd1f02294e3fac4c0c62941a3b828
sh1xzyy <alekslungol@gmail.com>
2025-10-06 23:21:31 +0300

    feat(OW2-371): add error content to show error content by its error type

--- src/ui/error/Error.tsx
+++ src/ui/error/Error.tsx
@@ -1,50 +1,47 @@
 import { type FC } from 'react';
 
-import GiraffeEating from './assets/giraffe-eating.svg';
-import Giraffe from './assets/giraffe.svg';
+import Link from 'next/link';
 
 import css from './styles/styles.module.scss';
 
 import Button from '../button/Button';
+import { errorContent } from './data/errorContent';
+import { ErrorType } from './shared/types/ErrorType';
 
-type Props = {
-  type: string;
-};
+const Error: FC<{ type: ErrorType }> = ({ type }) => {
+  console.log(type);
 
-const Error: FC<Props> = ({ type }) => {
-  const icon =
-    type === 'server' ? (
-      <GiraffeEating role="img" aria-label="giraffe" />
-    ) : (
-      <Giraffe role="img" aria-label="giraffe" />
-    );
+  const { titleLines, description, Icon, showUpdateButton } =
+    errorContent[type];
 
   return (
     <div className="container">
       <div className={css.wrapper}>
-        <div className={css.imageWrapper}>{icon}</div>
+        {Icon && (
+          <div className={css.imageWrapper}>
+            <Icon role="img" aria-label="giraffe" />
+          </div>
+        )}
+
         <div className={css.infoWrapper}>
-          {type === 'server' ? (
-            <>
-              <h2 className={css.title}>
-                Упс!
-                <br />
-                Щось пішло не так
-              </h2>
+          <h2 className={css.title}>
+            {titleLines.map((line, i) => (
+              <span key={i}>
+                {line}
+                {i < titleLines.length - 1 && <br />}
+              </span>
+            ))}
+          </h2>
 
-              <p className={css.description}>
-                Схоже, виникла неочікувана помилка. Ми вже знаємо про це й
-                працюємо над виправленням
-              </p>
-
-              <div className={css.buttonsWrapper}>
-                <Button label="Оновити сторінку" isUpdate={true} />
-                <Button label="Повернутися на головну" isHome={true} />
-              </div>
-            </>
-          ) : (
-            <div>No content yet</div>
-          )}
+          <p className={css.description}>{description}</p>
+          <div className={css.buttonsWrapper}>
+            {showUpdateButton && (
+              <Button label="Оновити сторінку" isUpdate={true} />
+            )}
+            <Link className={css.goHomeLink} href="/">
+              Повернутися на головну
+            </Link>
+          </div>
         </div>
       </div>
     </div>
