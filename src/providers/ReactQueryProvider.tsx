c1c90589b753eb77ed69c0df0c66529d141fa647
sh1xzyy <alekslungol@gmail.com>
2025-10-07 10:48:22 +0300

    refactor(OW2-298): update component typing to use FC<PropsWithChildren>

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { PropsWithChildren } from 'react';
+import type { FC, PropsWithChildren } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-export const ReactQueryProvider = ({ children }: PropsWithChildren) => {
+export const ReactQueryProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
