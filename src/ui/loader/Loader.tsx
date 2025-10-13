c7b1b0d03f5f01ef7082715c738a0adf04de147f
sh1xzyy <alekslungol@gmail.com>
2025-10-14 00:00:56 +0300

    feat(OW2-321): add loader

---
+++ src/ui/loader/Loader.tsx
@@ -0,0 +1,17 @@
+import { type FC } from 'react';
+
+import IconLoader from './assets/loader.svg';
+
+import './styles/Loader.scss';
+
+const Loader: FC = () => {
+  return (
+    <div className="loader-overlay">
+      <div className="loader-overlay__dot">
+        <IconLoader role="img" aria-label="loading" />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
