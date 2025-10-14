e65d1beb4324a37ba796022e46b7055ec59de770
sh1xzyy <alekslungol@gmail.com>
2025-10-14 23:39:38 +0300

    feat(OW2-314): add prev and next button and loop setting

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -8,12 +8,14 @@ import SliderItem from './ui/SliderItem/SliderItem';
 
 import { SLIDER_DATA } from './constants/sliderData';
 
+import Arrow from './assets/icons/arrow.svg';
+
 import './styles/Slider.scss';
 
 import { useDotButton } from './hooks/useDotButton';
 
 export const Slider: FC = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
   const { selectedIndex, scrollSnaps, onDotButtonClick } =
     useDotButton(emblaApi);
 
@@ -24,6 +26,34 @@ export const Slider: FC = () => {
           {SLIDER_DATA.map((data, i) => (
             <SliderItem key={data.id} data={data} index={i} />
           ))}
+        </div>
+      </div>
+
+      <div className="embla__buttons-wrapper">
+        <div className="container">
+          <button
+            className="embla__button left"
+            type="button"
+            onClick={() => emblaApi?.scrollPrev()}
+          >
+            <Arrow
+              className="embla__button-icon"
+              role="img"
+              aria-label="arrow"
+            />
+          </button>
+
+          <button
+            className="embla__button right"
+            type="button"
+            onClick={() => emblaApi?.scrollNext()}
+          >
+            <Arrow
+              className="embla__button-icon right"
+              role="img"
+              aria-label="arrow"
+            />
+          </button>
         </div>
       </div>
 
