ee43a991627de85f4982f678ba41d5df3b765ee2
Induktr <nikitavoitenko2020@gmail.com>
2025-11-01 22:45:31 +0200

    fix: apply review feedback to Checkbox component

--- src/ui/CheckBox/CheckBox.tsx
+++ src/ui/CheckBox/CheckBox.tsx
@@ -1,20 +1,25 @@
-"use client";
+'use client';
 
-import { InputHTMLAttributes, type FC } from 'react';
+import type { FC, InputHTMLAttributes, LabelHTMLAttributes } from 'react';
 
 import styles from './CheckBox.module.scss';
-interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement & HTMLLabelElement> {
+
+interface CheckBoxProps {
   labelText: string;
+  labelProps?: LabelHTMLAttributes<HTMLLabelElement>;
+  inputProps?: InputHTMLAttributes<HTMLInputElement>;
 }
 
-export const CheckBox: FC<CheckBoxProps> = ({ labelText, ...props }) => {
+export const CheckBox: FC<CheckBoxProps> = ({
+  labelText,
+  labelProps,
+  inputProps,
+}) => {
   return (
-    <label className={`${styles.check} ${styles.option}`} {...props}>
+    <label className={styles.check} {...labelProps}>
       {labelText}
-      <input className={styles.check__input} type="checkbox" {...props} />
+      <input className={styles.check__input} type="checkbox" {...inputProps} />
       <span className={styles.check__box} />
     </label>
   );
 };
-
-export default CheckBox;
