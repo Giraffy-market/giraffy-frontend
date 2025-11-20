86719c89da0bc98fb93a2e541245c2775a5df1c5
sh1xzyy <alekslungol@gmail.com>
2025-11-20 12:22:00 +0300

    chore(feature/OW2-301-auth): delete old constans folder

--- src/shared/api/constans/root.ts
+++
@@ -1,7 +0,0 @@
-export const makeEndpoints = (base: string) => ({
-  ROOT: base,
-  CREATE: base,
-  BY_ID: (id: string | number) => `${base}/${id}`,
-  UPDATE: (id: string | number) => `${base}/${id}`,
-  DELETE: (id: string | number) => `${base}/${id}`,
-});
