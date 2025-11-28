b685fbfd44ebbba35a9d65e2b13789fcd323d261
Gosyav <jegor.volkov.a@gmail.com>
2025-11-28 20:19:40 +0100

    fix: small fixes

--- src/components/Breadcrumbs/breadcrumbsData.ts
+++ src/components/Breadcrumbs/breadcrumbsData.ts
@@ -1,8 +1,9 @@
+import { routing } from '@/shared/routing';
+
 export const DICTIONARY: Record<string, string> = {
-  '/categories': 'Категорії',
+  [routing.categories.base]: 'Категорії',
   // TODO: Додати адресу та назви можливих сторінок
 };
 
-export const getDictionaryKey = (key: string): string => {
-  return DICTIONARY[`/${key}`] || key;
-};
+export const getDictionaryKey = (key: string): string =>
+  DICTIONARY[`/${key}`] || key;
