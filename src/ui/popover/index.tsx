dc8e3b17cb780a5f70ac06e5eb84b7acdc381b34
sh1xzyy <alekslungol@gmail.com>
2025-12-03 16:36:15 +0300

    feat(OW2-513): download shadcn Popover

---
+++ src/ui/popover/index.tsx
@@ -0,0 +1,40 @@
+import styles from "./styles.module.css";
+import type { ComponentProps } from "react";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
+
+function Popover({ ...props }: ComponentProps<typeof PopoverPrimitive.Root>) {
+  return <PopoverPrimitive.Root data-slot="popover" {...props} />;
+}
+
+function PopoverTrigger({
+  ...props
+}: ComponentProps<typeof PopoverPrimitive.Trigger>) {
+  return <PopoverPrimitive.Trigger data-slot="popover-trigger" {...props} />;
+}
+
+function PopoverContent({
+  className,
+  align = "center",
+  sideOffset = 4,
+  ...props
+}: ComponentProps<typeof PopoverPrimitive.Content>) {
+  return (
+    <PopoverPrimitive.Portal>
+      <PopoverPrimitive.Content
+        data-slot="popover-content"
+        align={align}
+        sideOffset={sideOffset}
+        className={`${styles["popover-content"]} ${className ?? ""}`.trim()}
+        {...props}
+      />
+    </PopoverPrimitive.Portal>
+  );
+}
+
+function PopoverAnchor({
+  ...props
+}: ComponentProps<typeof PopoverPrimitive.Anchor>) {
+  return <PopoverPrimitive.Anchor data-slot="popover-anchor" {...props} />;
+}
+
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor };
